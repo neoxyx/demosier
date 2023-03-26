7a1759b3a818ead140a548d717bffdb1d948f6fe
Jhon Jairo Valdés <jhon.valdes@softgic.co>
2023-03-26 12:07:22 -0500

    Test

--- src/app/services/post.service.spec.ts
+++ src/app/services/post.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { PostService } from './post.service';
 
 describe('PostService', () => {
@@ -8,9 +7,5 @@ describe('PostService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(PostService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
   });
 });
